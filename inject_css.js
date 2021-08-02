97c2a206ab1c968a9dadcb46563059e769a8590a
anemochore <fallensky@naver.com>
2021-08-02 23:57:17 +0900

    Update inject_css.js

--- inject_css.js
+++ inject_css.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Catan Universe Full Window 2021
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @updateURL    https://raw.githubusercontent.com/anemochore/Catan-Universe-Full-Window-2021/main/inject_css.js
 // @downloadURL  https://raw.githubusercontent.com/anemochore/Catan-Universe-Full-Window-2021/main/inject_css.js
 // @description  try to take over the world!
@@ -15,7 +15,7 @@
 const css = GM_getResourceText('CSS')
   .replace('@-moz-document url("https://catanuniverse.com/en/game/") {', '')
   .replace(/;/g, ' !important;')  //idk other ways to make it work
-  .trim().replace(/}$/, '');
+  .trim().replace(/}\n}$/, '}');
 
 GM_addStyle(css);
 console.log('css injected!');
